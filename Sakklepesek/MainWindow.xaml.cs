1ef68cb84810c89b53a47da07bcb1d25d08e284d
MihalyReka <mihajreka@gmail.com>
2022-05-18 19:35:26 +0200

    Felület generálása, javítása

--- Sakklepesek/MainWindow.xaml.cs
+++ Sakklepesek/MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace Sakklepesek
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+        Grid tabla;
+        Rectangle[,] tablaMezoi;
         public MainWindow()
         {
             InitializeComponent();
+            FeluletGeneralasa();
+            //SakktablaGeneralasa();
+        }
+
+        /*private void SakktablaGeneralasa()
+        {
+
+        }*/
+
+        private void FeluletGeneralasa()
+        { 
+        
+        babuk.Items.Add("Király");
+            babuk.Items.Add("Futo");
+            babuk.Items.Add("Bástya");
+            babuk.Items.Add("Huszár");
+            babuk.Items.Add("Sötét gyalog");
+            babuk.Items.Add("Világos gyalog");
+            babuk.Items.Add("Királynő");
+            tabla = new Grid(); 
+            tabla.HorizontalAlignment = HorizontalAlignment.Center;
+            tabla.VerticalAlignment = VerticalAlignment.Center;
+            Label aktualisHely = new Label();
+            ablak.Children.Add(tabla);
+            ablak.Children.Add(aktualisHely);
+            aktualisHely.Content = "A1";
+            aktualisHely.VerticalAlignment = VerticalAlignment.Bottom;
+            
+
+
         }
     }
 }
