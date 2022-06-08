3e7c4d143c40339b9a38ec418cce5b2eb58d348b
MihalyReka <mihajreka@gmail.com>
2022-06-08 22:35:10 +0200

    kisebb igazítások
    A tábla melletti számok és betűk létrehozása és rendezése.
    Adott mező rákattintásakor megjelenő kép megoldása.

--- Sakklepesek/MainWindow.xaml.cs
+++ Sakklepesek/MainWindow.xaml.cs
@@ -22,7 +22,10 @@ namespace Sakklepesek
     {
         
         Rectangle[,] tablaMezoi;
-        
+        char betuKorbeOldal;
+
+        int akty;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -33,9 +36,51 @@ namespace Sakklepesek
         private void SakktablaGeneralasa()
         {
             tablaMezoi = new Rectangle[10, 10];
-            for (int i = 0; i < 8; i++)
+            char betuKorbeOldal = 'A';
+            char betuKorbeSzel = 'A';
+
+
+            for (int i = 1; i < 9; i++)
             {
-                for (int j = 0; j < 8; j++)
+                
+                Label betu = new Label();
+                betu.HorizontalAlignment = HorizontalAlignment.Right;
+                betu.VerticalAlignment = VerticalAlignment.Top;
+                betu.Content = betuKorbeOldal++;
+                tabla.Children.Add(betu);
+                Grid.SetRow(betu, 9);
+                Grid.SetColumn(betu, i);
+
+                betu = new Label();
+                betu.HorizontalAlignment = HorizontalAlignment.Left;
+                betu.VerticalAlignment = VerticalAlignment.Center;
+                betu.Content = betuKorbeSzel++;
+                tabla.Children.Add(betu);
+                Grid.SetRow(betu, i);
+                Grid.SetColumn(betu, 9);
+            
+
+
+                //szamok
+                betu = new Label();
+                betu.HorizontalAlignment = HorizontalAlignment.Right;
+                betu.VerticalAlignment = VerticalAlignment.Center;
+                betu.Content = 9 - i;
+                tabla.Children.Add(betu);
+                Grid.SetRow(betu, 0);
+                Grid.SetColumn(betu, i);
+
+                betu = new Label();
+                betu.HorizontalAlignment = HorizontalAlignment.Left;
+                betu.VerticalAlignment = VerticalAlignment.Top;
+                betu.Content = 9 - i;
+                tabla.Children.Add(betu);
+                Grid.SetRow(betu, i);
+                Grid.SetColumn(betu, 0);
+            }
+            for (int i = 1; i < 9; i++)
+            {
+                for (int j = 1; j < 9; j++)
                 {
                     tablaMezoi[i, j] = new Rectangle();
                     tablaMezoi[i, j].Stroke = Brushes.Black;
@@ -55,9 +100,9 @@ namespace Sakklepesek
             
             int aktx = 0;
             int akty = 0;
-            for (int i = 0; i < 8; i++)
+            for (int i = 1; i < 9; i++)
             {
-                for (int j = 0; j < 8; j++)
+                for (int j = 1; j < 9; j++)
                 {
                     if (tablaMezoi[i, j].Equals(aktualis))
                     {
@@ -67,45 +112,59 @@ namespace Sakklepesek
                     }
                     tablaMezoi[i, j].Fill = (i + j) % 2 == 0 ? Brushes.White : Brushes.Black;
                 }
-            }
-
+            }           
             int sakkFigurak = babuk.SelectedIndex;
             if (sakkFigurak == 0)//Király
             {
-                tablaMezoi[aktx, akty].Fill = ;
-                tablaMezoi[aktx, akty].Fill = Brushes.Red;
-                tablaMezoi[aktx - 1, akty].Fill = Brushes.Red;
-                tablaMezoi[aktx - 1, akty - 1].Fill = Brushes.Red;
-                tablaMezoi[aktx - 1, akty + 1].Fill = Brushes.Red;
-                tablaMezoi[aktx, akty - 1].Fill = Brushes.Red;
-                tablaMezoi[aktx, akty + 1].Fill = Brushes.Red;
-                tablaMezoi[aktx + 1, akty].Fill = Brushes.Red;
-                tablaMezoi[aktx + 1, akty - 1].Fill = Brushes.Red;
-                tablaMezoi[aktx + 1, akty + 1].Fill = Brushes.Red;
-
+                   ImageBrush kiralyMinta = new ImageBrush();
+                    kiralyMinta.ImageSource = new BitmapImage(new Uri(@"C:\Users\User\Documents\GitHub\Sakklepesek_MihalyReka\Sakklepesek\Sakklepesek\Képek\kiraly.png", UriKind.Relative));
+                    tablaMezoi[aktx, akty].Fill = Brushes.Red;
+                    tablaMezoi[aktx, akty].Fill = kiralyMinta;
+                    tablaMezoi[aktx - 1, akty].Fill = Brushes.Red;
+                    tablaMezoi[aktx - 1, akty - 1].Fill = Brushes.Red;
+                    tablaMezoi[aktx - 1, akty + 1].Fill = Brushes.Red;
+                    tablaMezoi[aktx, akty - 1].Fill = Brushes.Red;
+                    tablaMezoi[aktx, akty + 1].Fill = Brushes.Red;
+                    tablaMezoi[aktx + 1, akty].Fill = Brushes.Red;
+                    tablaMezoi[aktx + 1, akty - 1].Fill = Brushes.Red;
+                    tablaMezoi[aktx + 1, akty + 1].Fill = Brushes.Red;
+               
             }
             else if (sakkFigurak == 1)//Futo
-            {
-              
-                for (int i = 0; i < 8; i++)
-                {
-                    //tablaMezoi[, ].Fill = Brushes.Red;
-                  
-                }
-                
+            {              
+                    for (int i = 1; i < 9; i++)
+                    {
+
+                        ImageBrush futoMinta = new ImageBrush();
+                        futoMinta.ImageSource = new BitmapImage(new Uri(@"C:\Users\User\Documents\GitHub\Sakklepesek_MihalyReka\Sakklepesek\Sakklepesek\Képek\futo.png", UriKind.Relative));
+                        tablaMezoi[aktx, akty].Fill = futoMinta;
+                        /*
+                             tablaMezoi[aktx+i,akty+i].Fill = Brushes.Red;
+                             tablaMezoi[aktx + i, akty - i].Fill = Brushes.Red;
+                             tablaMezoi[aktx -i, akty + i].Fill = Brushes.Red;
+                             tablaMezoi[aktx - i, akty - i].Fill = Brushes.Red;*/                       
+
+                    }
             }
             else if (sakkFigurak == 2)//Bástya
             {
-                for (int i = 0; i < 8; i++)
+                for (int i = 1; i < 9; i++)
                 {
+                    ImageBrush bastyaMinta = new ImageBrush();
+                    bastyaMinta.ImageSource = new BitmapImage(new Uri(@"C:\Users\User\Documents\GitHub\Sakklepesek_MihalyReka\Sakklepesek\Sakklepesek\Képek\bastya.jpg", UriKind.Relative));
+                    
                     tablaMezoi[aktx, i].Fill = Brushes.Red;
                     tablaMezoi[i, akty].Fill = Brushes.Red;
+                    tablaMezoi[aktx, akty].Fill = bastyaMinta;
                 }
             }
             else if (sakkFigurak == 3)//Huszár
             {
-                for (int i = 0; i < 8; i++)
+                for (int i = 1; i < 9; i++)
                 {
+                    ImageBrush huszarMinta = new ImageBrush();
+                    huszarMinta.ImageSource = new BitmapImage(new Uri(@"C:\Users\User\Documents\GitHub\Sakklepesek_MihalyReka\Sakklepesek\Sakklepesek\Képek\huszar.png", UriKind.Relative));
+                    
                     tablaMezoi[aktx, akty].Fill = Brushes.Red;
                     tablaMezoi[aktx + 2, akty + 1].Fill = Brushes.Red;
                     tablaMezoi[aktx + 2, akty - 1].Fill = Brushes.Red;
@@ -115,17 +174,21 @@ namespace Sakklepesek
                     tablaMezoi[aktx - 1, akty - 2].Fill = Brushes.Red;
                     tablaMezoi[aktx + 1, akty + 2].Fill = Brushes.Red;
                     tablaMezoi[aktx + 1, akty - 2].Fill = Brushes.Red;
+                    tablaMezoi[aktx, akty].Fill = huszarMinta;
                 }
             }
             else if (sakkFigurak == 4)//Sötét gyalog
             {
-                if (aktx == 6)
+                ImageBrush gyalogSMinta = new ImageBrush();
+                gyalogSMinta.ImageSource = new BitmapImage(new Uri(@"C:\Users\User\Documents\GitHub\Sakklepesek_MihalyReka\Sakklepesek\Sakklepesek\Képek\sGyalog.png", UriKind.Relative));
+                tablaMezoi[aktx, akty].Fill = gyalogSMinta;
+                if (aktx == 7)
                 {
                     tablaMezoi[aktx, akty].Fill = Brushes.Red;
                     tablaMezoi[aktx - 1, akty].Fill = Brushes.Red;
                     tablaMezoi[aktx - 2, akty].Fill = Brushes.Red;
                 }
-                else if (aktx == 0)
+                else if (aktx == 1)
                 {
                     tablaMezoi[aktx, akty].Fill = Brushes.Red;
                 }
@@ -134,16 +197,20 @@ namespace Sakklepesek
                     tablaMezoi[aktx, akty].Fill = Brushes.Red;
                     tablaMezoi[aktx - 1, akty].Fill = Brushes.Red;
                 }
+                tablaMezoi[aktx, akty].Fill = gyalogSMinta;
             }
             else if (sakkFigurak == 5)//Világos gyalog
             {
-                if (aktx==1)
+                ImageBrush gyalogVMinta = new ImageBrush();
+                gyalogVMinta.ImageSource = new BitmapImage(new Uri(@"C:\Users\User\Documents\GitHub\Sakklepesek_MihalyReka\Sakklepesek\Sakklepesek\Képek\vGyalog.png", UriKind.Relative));
+                
+                if (aktx==2)
                 {
                     tablaMezoi[aktx, akty ].Fill = Brushes.Red;
                     tablaMezoi[aktx+1 , akty ].Fill = Brushes.Red;
                     tablaMezoi[aktx +2, akty].Fill = Brushes.Red;
                 }
-                else if(aktx == 7)
+                else if(aktx == 8)
                 {
                     tablaMezoi[aktx, akty].Fill = Brushes.Red;
                 }
@@ -152,21 +219,30 @@ namespace Sakklepesek
                     tablaMezoi[aktx, akty].Fill = Brushes.Red;
                     tablaMezoi[aktx+1, akty ].Fill = Brushes.Red;
                 }
-                
-                
+                tablaMezoi[aktx, akty].Fill = gyalogVMinta;
+
+
             }
             else if (sakkFigurak == 6)//Királynő
-            {
-                for (int i = 0; i < 8; i++)
-                {
+            {                         
+                    for (int i = 1; i < 9; i++)
+                    {
+                    
+                    
+                        ImageBrush kiralynoMinta = new ImageBrush();
+                        kiralynoMinta.ImageSource = new BitmapImage(new Uri(@"C:\Users\User\Documents\GitHub\Sakklepesek_MihalyReka\Sakklepesek\Sakklepesek\Képek\kiralyno.png", UriKind.Relative));
+                       
                         tablaMezoi[aktx, i].Fill = Brushes.Red;
                         tablaMezoi[i, akty].Fill = Brushes.Red;
+                        /*
+                        tablaMezoi[aktx+i, akty+i].Fill = Brushes.Red;
+                        tablaMezoi[aktx + i, akty - i].Fill = Brushes.Red;
+                        tablaMezoi[aktx -i, akty + i].Fill = Brushes.Red;
+                        tablaMezoi[aktx - i, akty - i].Fill = Brushes.Red;*/                       
+                        tablaMezoi[aktx, akty].Fill = kiralynoMinta;
+                    }
                 
-                }
-            }
-
-
-
+            }          
         }
         private void FeluletGeneralasa()
         {
@@ -183,10 +259,10 @@ namespace Sakklepesek
                 tabla.RowDefinitions.Add(new RowDefinition());
                 tabla.ColumnDefinitions.Add(new ColumnDefinition());
             }
-            
+
             Label aktualisHely = new Label();
             ablak.Children.Add(aktualisHely);
-            aktualisHely.Content = "A1";
+            aktualisHely.Content ="A1";
             aktualisHely.VerticalAlignment = VerticalAlignment.Bottom;
             
 
